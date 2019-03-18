121dffcc06923c59ee3d30620794fed679a5eaa8
zhousir <2919150@qq.com>
2019-03-18 16:09:56 +0800

    表格提示

--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="emp_contaner">
-    <table>
-      <tr>
-        <th>头像</th>
-        <th>用户名</th>
-        <th>密码</th>
-        <th colspan="2">操作</th>
-      </tr>
-
-      <tr v-for="(user,index) in userLists" :key="index">
-        <!-- <td v-if="user.headerimg">
-          <img :src="BASEURL + user.headerimg" width="80px" height="80px">
-        </td>
-        <td v-else>
-          <img src="../../assets/images/y.jpg" width="80px" height="80px">
-        </td> -->
-        <!-- 在学习过程中，遇到不明白，先放一下，作好记录，后期遇到在处理; -->
-        <td>
-          <img v-if="user.headerimg" :src="BASEURL + user.headerimg" width="80px" height="80px"/>
+    <el-table
+      :data="userLists"
+       border
+      style="width: 100%">
+      <el-table-column
+        label="头像"
+        width="300">
+        <template slot-scope="scope">
+          <img v-if="scope.row.headerimg" :src="BASEURL + scope.row.headerimg" width="80px" height="80px"/>
           <img v-else src="../../assets/images/y.jpg" width="80px" height="80px"/>
-        </td>
+        </template>
 
-        <td>{{user.username}}</td>
-        <td>{{user.password}}</td>
-        <td>
-          <router-link :to="{path:'user_list',query:{uid:user.uid}}" @click.native="userDelete">删除</router-link>
-        </td>
-        <td>
-          <router-link :to="{path:'user_update',query:{uid: user.uid}}">修改</router-link>
-        </td>
-      </tr>
-    </table>
+      </el-table-column>
+      <el-table-column
+        label="用户名"
+        width="300">
+        <template slot-scope="scope">
+            <span>{{scope.row.username}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="密码"
+        width="300">
+         <template slot-scope="scope">
+            <span>{{scope.row.password}}</span>
+        </template>
+      </el-table-column>
+
+       <el-table-column
+        label="操作"
+        width="400">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-delete"  @click="userDelete(scope.row.uid)" >删除</el-button>
+          <el-button type="primary" icon="el-icon-edit" @click="userUpdate(scope.row.uid)"  >修改</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
@@ -57,14 +63,33 @@ export default {
         }
       });
     },
-    userDelete() {
-      let uid = this.$route.query.uid;
-      const params = { params: { uid: uid } };
-      RequestUserDelete(params).then(data => {
-        if (data.resultCode === 1) {
-          this.userList();
-        }
-      });
+    userDelete(id) {
+        this.$confirm('确认删除该记录吗?', '提示', {
+                  confirmButtonText: '确定',
+                  cancelButtonText: '取消',
+                  type: 'warning'
+                }).then(() => {
+
+                      const params = { params: { uid: id } };
+                      RequestUserDelete(params).then(data => {
+                        if (data.resultCode === 1) {
+                          this.userList();
+                            this.$message({
+                              type: 'success',
+                              message: '删除成功!'
+                            });
+                        }
+                      });
+             
+                }).catch(() => {
+                  this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                  });          
+                });
+    },
+    userUpdate(id){
+       this.$router.push({path:'user_update',query:{uid: id}});
     }
   }
 };
